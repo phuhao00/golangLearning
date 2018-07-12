7c075689595a0a848990c1bba8badaaf540d24d0
phuhao00 <phuhao00@users.noreply.github.com>
2018-07-12 17:03:12 +0800

    Create go_test.go

---
+++ go_test.go
@@ -0,0 +1,24 @@
+package awesomeProject
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestReturn(t *testing.T) {
+	a,b:=test()
+
+	fmt.Printf("%d--%d\n",a,b)
+	c,d:=test2()
+	fmt.Printf("%d--%d",c,d)
+
+}
+func test() (int32,int32) {
+	return 1,2
+}
+
+func test2() (a int32,b int32) {
+	a=3
+	b=4
+	return
+}
