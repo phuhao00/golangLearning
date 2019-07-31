b0bb3e4bc7ae1841e459454a6d3c3534bc207771
huhao <1559531623@qq.com>
2019-07-31 15:45:25 +0800

    slice range

---
+++ ty/g.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main()  {
+	//23点输出23
+	fmt.Println(time.Now().Hour())
+}
---
+++ yy/test.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	hh:=[]int32{1,2,3,4,5,6,7,8}
+	for _, value := range hh {
+		fmt.Println(value)
+	}
+}
